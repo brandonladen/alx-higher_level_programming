e8e7b558136622dd266afae297a05ca8fce4cbd7
brandonladen <brandonladen486@gmail.com>
2023-06-06 03:33:37 -0700

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x < 10:
-        print("0{}".format(x), end="")
+    if x == 99:
+        print("{}".format(x))
     else:
-        print("{}".format(x), end="")
-    if x != 99:
-        print(", ", end="")
-print()
+        print("{:02}".format(x), end=", ")
