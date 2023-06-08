0205c4a8c8df2b26e5fafcd7e6cd74697382efcc
brandonladen <brandonladen486@gmail.com>
2023-06-08 09:41:33 -0700

    created 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) <= 3:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        Total = 0
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = sys.argv[2]
+        if sys.argv[2] == "+":
+            Total = add(a, b)
+            print("{} {} {} = {}".format(a, operator, b, Total))
+        elif sys.argv[2] == "-":
+            Total = sub(a, b)
+            print("{} {} {} = {}".format(a, operator, b, Total))
+        elif sys.argv[2] == "*":
+            Total = mul(a, b)
+            print("{} {} {} = {}".format(a, operator, b, Total))
+        elif sys.argv[2] == "/":
+            Total = div(a, b)
+            print("{} {} {} = {}".format(a, operator, b, Total))
+        else:
+            print("Unknown operator. Available operatos: +, -, * and /")
+            sys.exit(1)
