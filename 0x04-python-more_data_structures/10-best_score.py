cc1f97a81dc27010841b1561ced1d741a48ccf62
brandonladen <brandonladen486@gmail.com>
2023-06-14 13:26:49 -0700

    removed linting error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     H_score = -100
     name = None
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary == name or len(a_dictionary) == 0:
         return None
     for k, v in a_dictionary.items():
         if v > H_score:
