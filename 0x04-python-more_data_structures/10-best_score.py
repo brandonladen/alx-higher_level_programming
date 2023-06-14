43ed4887472ca6a95cc4e83ce4217a1c9d9512c0
brandonladen <brandonladen486@gmail.com>
2023-06-14 13:01:07 -0700

    removed bugs

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    H_score = 0
-    name = ""
-    if a_dictionary == None:
+    H_score = -100
+    name = None
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     for k, v in a_dictionary.items():
         if v > H_score:
