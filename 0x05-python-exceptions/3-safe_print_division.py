c7d7e5fdd50dc29a4a4c49f6a254d0ed0fd07318
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-06-26 16:48:24 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""
+ divides 2 integers and prints the result.
+    """
+
+
 def safe_print_division(a, b):
     try:
-        div = int(a / b)
-    except (ZeroDivisionError,TypeError):
-        div = (None)
+        x = a / b
+    except:
+        x = None
     finally:
-        print("Inside results: {:d}".format(div))
-        return(div)
+        print("Inside result: {}".format(x))
+        return x
