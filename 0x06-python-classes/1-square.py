8a98e87256a9539ac8a036e50e943ebaf5a750d4
brandonladen <brandonladen486@gmail.com>
2023-06-27 03:23:00 -0700

    create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module Square """
+
+class Square:
+    """ Defines this class """
+    def __init__(self, size):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
