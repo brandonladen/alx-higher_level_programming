9ad3a8c01efa281677db4a79311cc12bf35a0df5
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-07-10 21:49:06 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry():
                 name: Is always a string
                 value: To be validated
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
