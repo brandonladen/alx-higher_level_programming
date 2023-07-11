aefeb47beea6d5832dd312af437f2951e07b0a7c
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-07-11 20:57:31 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,5 +4,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, mode="r", encoding="utf=8") as myFile:
+    with open(filename, "r", encoding="utf=8") as myFile:
         return json.loads(myFile)
