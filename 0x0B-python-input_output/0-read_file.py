8ceed47775fe74a0c32ec05b5a0ae439b84ae20d
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-07-11 20:53:45 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
         Returns:
             Prints the text to the standard output
     """
-    with open(filename, mode="r", encoding="utf-8") as f:
-            print(f.read())
+    with open(filename, "r", encoding="utf-8") as f:
+            print(f.read(), end="")
