db15deaba11720d2e54c39d28a8da815ac0d9029
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-08-16 22:58:34 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- a script that lists all preveleges of the MySQL users
+-- user_0d_1 on your server (in localhost)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- user_0d_2 on your server (in localhost)
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
