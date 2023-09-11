02f55b8c2bbd799d4e445da9584357855a93e8aa
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-09-12 01:08:27 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (n < 0) {
-        return (-1);
-    }
-    if (n === 0 || isNaN(n)) {
-        return (1);
-    }
-    return (n * factorial(n - 1));
+function factorialize (num) {
+  let MyRes = 1;
+  for (let i = 1; i <= num; i++) {
+    MyRes *= i;
+  }
+  return (MyRes);
 }
-
-console.log(factorial(Number(process.argv[2])));
+console.log(factorialize(parseInt(process.argv[2])));
